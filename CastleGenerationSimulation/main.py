bd65f4abeda2d075184ebe873da9873aac425c63
Dexter Reese <Ehlers.Jakob@gmail.com>
2025-10-24 12:40:04 +0200

    Signed-off-by: Dexter Reese <Ehlers.Jakob@gmail.com>

--- CastleGenerationSimulation/main.py
+++ CastleGenerationSimulation/main.py
@@ -1,5 +1,6 @@
 import pygame
 import sys
+from Level import Level
 
 print("Hello world")
 # Initialize pygame
@@ -13,6 +14,19 @@ pygame.display.set_caption("Fortify Simulation")
 # Define a grass-green color (R, G, B)
 GREEN = (34, 139, 34)
 
+lvl = Level().getLevel()
+print()
+cell_size = 8
+
+def chaosDisplay(lvl):
+
+    for r in range(len(lvl)):
+        for c in range(len(lvl[0])):
+            lvl
+            color = lvl[r][c],lvl[r][c],lvl[r][c]
+            rect = pygame.Rect(c * cell_size, r * cell_size, cell_size, cell_size)
+            pygame.draw.rect(screen, color, rect)
+
 # Main loop
 running = True
 while running:
@@ -22,7 +36,8 @@ while running:
 
     # Fill the background with green
     screen.fill(GREEN)
-
+    chaosDisplay(lvl)
+    #swap buffer
     pygame.display.flip()
 
 # Quit pygame cleanly
