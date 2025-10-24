72a60c0a1655d1de3e1d9d8b4c0bd3b546e588ed
oljh <oljh@itu.dk>
2025-10-24 12:48:09 +0200

    add files to model

---
+++ CastleGenerationSimulation/simulation.py
@@ -0,0 +1,15 @@
+from initializationParameters import InitializationParameters
+
+
+class Simulation:
+    def __init__(self, initParams: InitializationParameters):
+        pass
+
+    def step(self):
+        pass
+
+    def getState(self):
+        pass
+
+    def runSimulation(self):
+        pass
