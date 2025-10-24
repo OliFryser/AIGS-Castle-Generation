b80221368ffc3de55bb4538b1296a058ff007097
oljh <oljh@itu.dk>
2025-10-24 14:28:45 +0200

    load level from file

---
+++ CastleGenerationSimulation/HelperScripts/LevelGeneratorScript.py
@@ -0,0 +1,34 @@
+import random
+import sys
+
+
+def main():
+    if len(sys.argv) != 5:
+        print(
+            "Usage: python generate_numbers.py <width> <height> <max_height> <output_file>"
+        )
+        sys.exit(1)
+
+    try:
+        width = int(sys.argv[1])
+        height = int(sys.argv[2])
+        max_height = int(sys.argv[3])
+        output_file = sys.argv[4]
+    except ValueError:
+        print("width, height and max_height must be integers.")
+        sys.exit(1)
+
+    with open(output_file, "w") as f:
+        line = f"{width} {height} {max_height}"
+        f.write(line + "\n")
+        for _ in range(height):
+            line = " ".join(str(random.randint(0, max_height)) for _ in range(width))
+            f.write(line + "\n")
+
+    print(
+        f"File '{output_file}' generated with {height} lines and {width} numbers per line."
+    )
+
+
+if __name__ == "__main__":
+    main()
--- CastleGenerationSimulation/Level.py
+++ CastleGenerationSimulation/Level.py
@@ -3,12 +3,21 @@ import numpy as np
 
 class Level:
     def __init__(self, levelFilepath: str):
-        self.height = 60
-        self.width = 80
-        self.createLevel()
+        self.createLevel(levelFilepath)
 
-    def createLevel(self):
-        self.level = np.random.randint(0, 256, (self.height, self.width))
+    def createLevel(self, levelFilepath: str):
+        with open(levelFilepath, "r") as f:
+            self.width, self.height, self.max_height = [
+                int(x) for x in f.readline().rstrip().split()
+            ]
+            self.level = np.zeros((self.height, self.width))
+            for y in range(self.height):
+                line = [int(num) for num in f.readline().rstrip().split()]
+                for x in range(self.width):
+                    self.level[y][x] = line[x]
 
     def getLevel(self):
         return self.level
+
+    def getCell(self, x, y):
+        return self.level[y][x]
