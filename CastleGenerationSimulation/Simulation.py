bba1e7bc0fb22a04610e7207b450f88df2108435
Dexter Reese <Ehlers.Jakob@gmail.com>
2025-10-31 22:59:25 +0100

    simulation got node graph to pass down to units

--- CastleGenerationSimulation/Simulation.py
+++ CastleGenerationSimulation/Simulation.py
@@ -3,6 +3,7 @@ from Target import Target
 from Level import Level
 from InitializationParameters import InitializationParameters
 from Unit import Unit
+from Utils.Node import createNodeGraph
 
 
 class Simulation:
@@ -10,11 +11,12 @@ class Simulation:
         self.level = Level(
             initParams.levelFilepath, initParams.castleGenerationFilepath
         )
-
+        self.units = []
+        self.updateNodeGraph()
         # these are test things
-        unit = Unit(self.level, Vector2(20, 80))
-        unit0 = Unit(self.level, Vector2(10, 50))
-        unit1 = Unit(self.level, Vector2(30, 10))
+        unit = Unit(self.ng,self.level, Vector2(20, 80))
+        unit0 = Unit(self.ng,self.level, Vector2(10, 50))
+        unit1 = Unit(self.ng,self.level, Vector2(30, 10))
         self.target = Target(
             self.level, Vector2(self.level.width / 2, self.level.height / 2)
         )
@@ -32,3 +34,9 @@ class Simulation:
 
     def runSimulation(self):
         pass
+
+    def updateNodeGraph(self):
+        self.ng = createNodeGraph(self.level)
+    
+        for unit in self.units:
+            unit.ng = self.ng
