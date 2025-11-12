c108ec3d1b4631e23a35e941083ecf63a02af4db
OliFryser <oliver-1999@live.dk>
2025-11-12 15:50:46 +0100

    add filepath wrapper to allow to run main.py from project root

--- CastleGenerationSimulation/Simulation.py
+++ CastleGenerationSimulation/Simulation.py
@@ -9,18 +9,20 @@ from Utils.Node import Graph
 class Simulation:
     def __init__(self, initParams: InitializationParameters):
         self.level = Level(
-            initParams.levelFilepath, initParams.castleGenerationFilepath, initParams.castleTilesFilepath
-        )   
+            initParams.levelFilepath.getFilepath(),
+            initParams.castleGenerationFilepath.getFilepath(),
+            initParams.castleTilesFilepath.getFilepath(),
+        )
         self.units = []
         self.updateNodeGraph()
         # these are test things
-        unit = AxeMan(self.ng,self.level, Vector2(20, 80))
-        unit0 = AxeMan(self.ng,self.level, Vector2(10, 50))
-        unit1 = AxeMan(self.ng,self.level, Vector2(30, 10))
+        unit = AxeMan(self.ng, self.level, Vector2(20, 80))
+        unit0 = AxeMan(self.ng, self.level, Vector2(10, 50))
+        unit1 = AxeMan(self.ng, self.level, Vector2(30, 10))
         self.target = Target(
-            self.level, Vector2(self.level.width / 2 +2.5, self.level.height / 2 -1.)
+            self.level, Vector2(self.level.width / 2 + 2.5, self.level.height / 2 - 1.0)
         )
-        
+
         self.units = [unit, unit0, unit1]
         for u in self.units:
             u.goal = self.target.position
@@ -38,7 +40,6 @@ class Simulation:
         print(n)
         """
 
-
     def getState(self):
         pass
 
@@ -48,7 +49,7 @@ class Simulation:
     def updateNodeGraph(self):
         graphs = [
             Graph(self.level),
-            #createHexNodeGrap(self.level),
+            # createHexNodeGrap(self.level),
         ]
         self.ng = graphs[0]
         for unit in self.units:
